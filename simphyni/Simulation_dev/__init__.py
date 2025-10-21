c256d65271dd328bef939250e4460c608b4c169a
Ishaq <iobal@mit.edu>
2025-10-21 10:37:15 -0400

    deleted unneccesary files

--- simphyni/Simulation_dev/__init__.py
+++
@@ -1,8 +0,0 @@
-from .tree_simulator import TreeSimulator
-# from .SimulationMethods import simulate_glrates
-from .PairStatistics import PairStatistics
-
-__all__ = [
-    "TreeSimulator",
-    "PairStatistics"
-]
